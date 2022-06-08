0726790c210c161f6f9a10280e34a9135b93e425
melaniemdm <melanieborja.pro@gmail.com>
2022-06-08 10:59:13 +0200

    feature(js&css) all graph with recharts

--- src/pages/profil/index.js
+++ src/pages/profil/index.js
@@ -1,18 +1,27 @@
-import './style.scss';
-import clappingTxt from '../../assets/clappingTxt.png';
-
-function Profil(){
-    const name= "Thomas";
-    return(
-      <div className="containerFirst">
-      <div className="titleProfil">Bonjour &nbsp;<div className="name">{name}</div></div>
+import "./style.scss";
+import clappingTxt from "../../assets/clappingTxt.png";
+import DailyActivity from "../../components/DailyActivity";
+import AverageSession from "../../components/AverageSession";
+import Score from "../../components/Score";
+import Intensity from "../../components/Intensity";
+function Profil() {
+  const name = "Thomas";
+  return (
+    <div className="containerFirst">
+      <div className="titleProfil">
+        Bonjour &nbsp;<div className="name">{name}</div>
+      </div>
       <div className="txtProfil">
-      <img src={clappingTxt} alt="logo clapping du texte" />
+        <img src={clappingTxt} alt="logo clapping du texte" />
       </div>
+      <DailyActivity />
+      <div className="littleGraph">
+        <AverageSession />
+        <Score />
+        <Intensity />
       </div>
-     
-     
-    );
-  }
-  
-  export default Profil;
+    </div>
+  );
+}
+
+export default Profil;
