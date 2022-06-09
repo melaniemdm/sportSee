c720f67d0fe046fe6cae8e5ef35385fb386f0afc
melaniemdm <melanieborja.pro@gmail.com>
2022-06-09 10:53:40 +0200

    fix(img) bodyMass feature(js) props fix(js) prfilPage creatArrayKeydata

--- src/components/BodyMassIndex/index.js
+++ src/components/BodyMassIndex/index.js
@@ -1,11 +1,19 @@
 import fire from "../../assets/fire.png";
 import './style.scss';
+import {getUser} from '../../utils/data';
 
-function BodyMassIndex(){
+
+function BodyMassIndex(props){
+    const bodyMass = getUser(18).data.keyData;
+    console.log(bodyMass)
+    
     return (
         <div className="bodyMassIndex">
             <div className="containerIcone">
                 <img src={fire} alt="icone"/>
+                <div className="bodyMass calories">{props.data.value} {props.data.type}</div>
+
+               
             </div>
         </div>
     )
--- src/pages/profil/index.js
+++ src/pages/profil/index.js
@@ -5,11 +5,21 @@ import AverageSession from "../../components/AverageSession";
 import Score from "../../components/Score";
 import Intensity from "../../components/Intensity";
 import BodyMassIndex from "../../components/BodyMassIndex";
-import {getUser} from '../../utils/data';
+import { getUser } from "../../utils/data";
+
+const keyData = getUser(18).data.keyData;
+console.log(keyData);
+
+const bodyMassIndex = []
+bodyMassIndex.push({value:keyData.calorieCount, type:"Calories"} )
+bodyMassIndex.push({value:keyData.proteinCount, type:"Protéines"} )
+bodyMassIndex.push({value:keyData.carbohydrateCount, type:"Glucides"} )
+bodyMassIndex.push({value:keyData.lipidCount, type:"Lipides"} )
 
 function Profil() {
   const name = getUser(18).data.userInfos.firstName;
-  console.log(name)
+  console.log(name);
+
   return (
     <div className="containerFirst">
       <div className="titleProfil">
@@ -18,19 +28,26 @@ function Profil() {
       <div className="txtProfil">
         <img src={clappingTxt} alt="logo clapping du texte" />
       </div>
-      <div className="dailyActivity"><DailyActivity /></div>
+      <div className="dailyActivity">
+        <DailyActivity />
+      </div>
       <div className="littleGraph">
-      <div className="containeRed"> <AverageSession /></div> 
-      <div className="containerBlack"><Intensity /></div> 
-        <div className="containerGray score"><Score /></div> 
+        <div className="containeRed">
+          {" "}
+          <AverageSession />
         </div>
+        <div className="containerBlack">
+          <Intensity />
+        </div>
+        <div className="containerGray score">
+          <Score />
+        </div>
+      </div>
 
-        <div className="containerBody">
-          <BodyMassIndex/>
-          <BodyMassIndex/>
-          <BodyMassIndex/>
-          <BodyMassIndex/>
-          </div>
+      <div className="containerBody">
+        {bodyMassIndex.map(item=><BodyMassIndex data={item} /> )}
+        
+      </div>
     </div>
   );
 }
