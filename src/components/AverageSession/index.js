52de8f64b8cdc138a3eb55e0991a116489a5c97d
melaniemdm <melanieborja.pro@gmail.com>
2022-06-14 11:54:52 +0200

    feature(js) axios average

--- src/components/AverageSession/index.js
+++ src/components/AverageSession/index.js
@@ -1,32 +1,45 @@
 import './style.scss';
-import React from "react";
 import {
   LineChart,
   Line,
   XAxis,
   Tooltip,
  } from "recharts";
-import {getEverageSessions} from '../../utils/data';
+//import {getEverageSessions} from '../../utils/data';
+//const dataEverageSessions = getEverageSessions(18).data.sessions;
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const dataEverageSessions = getEverageSessions(18).data.sessions;
-const everageSessions = dataEverageSessions.map(everageSession=> { 
-  return{
-    name: everageSession.name,
-    day: everageSession.day,
-    pv: 3800,
-      
-  }
- 
-})
 
 
 export default function AverageSession() {
+  
+const [averageSessions,setAverageSessions]= useState([]);
+
+const getData = async () => {
+  const arrayDay =["L", "M", "M", "J", "V", "S","D"];
+  const { data } = await axios.get(`http://localhost:3000/user/12/average-sessions`);
+const averageSessionsArray = data.data.sessions.map(averageSession=> { 
+  return{
+    name: arrayDay[averageSession.day -1],
+    pv: averageSession.sessionLength,
+  }
+ 
+})
+setAverageSessions(averageSessionsArray)
+console.log(averageSessionsArray)
+}
+ 
+useEffect(() => {
+  getData();
+}, []);
+
   return (<div>
   <div className="containerTitleGraphRed">Durée moyenne des sessions</div>
     <LineChart
       width={263}
       height={258}
-      data={everageSessions}
+      data={averageSessions}
       
     >
       
@@ -35,7 +48,7 @@ export default function AverageSession() {
       <Tooltip />
      
       
-      <Line type="monotone" dataKey="day" stroke="black" />
+      <Line type="monotone" dataKey="pv" stroke="black" />
     </LineChart></div>
   );
 }
