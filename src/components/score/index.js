92007220759cca1661f0179951eaa3b2d5ec1836
melaniemdm <melanieborja.pro@gmail.com>
2022-06-15 09:31:14 +0200

    feature(js) doc score

--- src/components/score/index.js
+++ src/components/score/index.js
@@ -3,13 +3,31 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { PieChart, Pie, Cell } from "recharts";
 import { useParams } from 'react-router-dom';
-// const score = 0.3;
 
-
-
+/**
+ * I'm getting data from an API and then I'm setting the data to a variable called score.
+ * </code>
+ * 
+ * 
+ * A:
+ * 
+ * You can use <code>useMemo</code> hook to memoize the data.
+ * <code>const dataScore = useMemo(() =&gt; {
+ *   const scoreUser = data.data.todayScore ? data.data.todayScore : data.data.score;
+ *   return [
+ *     { name: "Group A", value: scoreUser },
+ *     { name: "Group B", value: 1 - scoreUser },
+ *   ];
+ * }, [data]);
+ * </code>
+ * @returns A pie chart with two slices.
+ */
 export default function Score() {
   const {id} = useParams();
   const [score, setScore] = useState([]);
+  /**
+   * I'm getting data from an API and then I'm setting the data to a variable called score.
+   */
   const getData = async () => {
   const { data } = await axios.get(`http://localhost:3000/user/` + id);
 const scoreUser = data.data.todayScore ? data.data.todayScore : data.data.score;
@@ -21,6 +39,7 @@ setScore(dataScore)
 console.log(dataScore)
   }
 
+ /* It's a hook that is called when the component is mounted. */
   useEffect(() => {
     getData();
   });
