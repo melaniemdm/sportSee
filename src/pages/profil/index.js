53cb88699525c8449b93b0e5acd155d6ba03af90
melaniemdm <melanieborja.pro@gmail.com>
2022-06-15 09:36:52 +0200

    feature(js) pages-profil

--- src/pages/profil/index.js
+++ src/pages/profil/index.js
@@ -5,47 +5,65 @@ import AverageSession from "../../components/AverageSession";
 import Score from "../../components/Score";
 import Intensity from "../../components/Intensity";
 import BodyMassIndex from "../../components/BodyMassIndex";
-import fire from '../../assets/fire.png';
-import prot from '../../assets/prot.png';
-import apple from '../../assets/apple.png';
-import lipides from '../../assets/lipides.png';
+import fire from "../../assets/fire.png";
+import prot from "../../assets/prot.png";
+import apple from "../../assets/apple.png";
+import lipides from "../../assets/lipides.png";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
-
+/**
+ * I'm using axios to get data from my backend, then I'm setting the state of my component with the
+ * data I got from my backend.
+ * </code>
+ * @returns A React component.
+ */
 function Profil() {
- 
-
-  //const name = getUser(18).data.userInfos.firstName;
-  const {id} = useParams();
- const [name, setName] = useState([]);
- const [bodyMassIndex,setBodyMassIndex]= useState([]);
+  const { id } = useParams();
+  const [name, setName] = useState([]);
+  const [bodyMassIndex, setBodyMassIndex] = useState([]);
+  /**
+   * I'm using axios to get data from my backend, then I'm setting the state of my component with the
+   * data I got from my backend.
+   */
   const getData = async () => {
-    const { data } = await axios.get(`http://localhost:3000/user/`+id);
+    const { data } = await axios.get(`http://localhost:3000/user/` + id);
     setName(data.data.userInfos.firstName);
-  const keyData = data.data.keyData;
+    const keyData = data.data.keyData;
 
-    let bmiArray =[]
-    bmiArray.push({ value:keyData.calorieCount, type:"Calories", picture:fire} )
-    bmiArray.push({value:keyData.proteinCount, type:"Protéines", picture:prot} )
-    bmiArray.push({value:keyData.carbohydrateCount, type:"Glucides",picture:apple} )
-    bmiArray.push({value:keyData.lipidCount, type:"Lipides", picture:lipides} )
-    setBodyMassIndex(bmiArray)
+    let bmiArray = [];
+    bmiArray.push({
+      value: keyData.calorieCount,
+      type: "Calories",
+      picture: fire,
+    });
+    bmiArray.push({
+      value: keyData.proteinCount,
+      type: "Protéines",
+      picture: prot,
+    });
+    bmiArray.push({
+      value: keyData.carbohydrateCount,
+      type: "Glucides",
+      picture: apple,
+    });
+    bmiArray.push({
+      value: keyData.lipidCount,
+      type: "Lipides",
+      picture: lipides,
+    });
+    setBodyMassIndex(bmiArray);
   };
+  /* It's a hook that is called when the component is mounted. */
   useEffect(() => {
     getData();
   });
- 
-  
+
   return (
     <div className="containerFirst">
-            
-
-    
       <div className="titleProfil">
-    
-        Bonjour &nbsp;<div className="name" >{name}</div>
+        Bonjour &nbsp;<div className="name">{name}</div>
       </div>
       <div className="txtProfil">
         <img src={clappingTxt} alt="logo clapping du texte" />
@@ -67,8 +85,9 @@ function Profil() {
       </div>
 
       <div className="containerBody">
-        {bodyMassIndex.map((item,index)=><BodyMassIndex key={index} data={item} /> )}
-        
+        {bodyMassIndex.map((item, index) => (
+          <BodyMassIndex key={index} data={item} />
+        ))}
       </div>
     </div>
   );
