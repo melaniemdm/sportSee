0d946631800b9f789e51386bb3732cf9053707a1
melaniemdm <melanieborja.pro@gmail.com>
2022-06-15 09:25:08 +0200

    feature(js) documentation bodymass

--- src/components/BodyMassIndex/index.js
+++ src/components/BodyMassIndex/index.js
@@ -3,6 +3,16 @@ import './style.scss';
 // import {getUser} from '../../utils/data';
 
 
+/**
+ * It's a function that returns a div with a class name of bodyMassIndex. Inside that div, there's
+ * another div with a class name of containerIconeBodyMass. Inside that div, there's another div with a
+ * class name of containerIconeBodyMass. Inside that div, there's an image with a source of
+ * props.data.picture, a class name of iconeBodyMass, and an alt of icone.
+ * 
+ * After that, there's another div with a class name of bodyMass calories. Inside that div, there's a
+ * value of props.data.value and a type of props.data.type.
+ * @param props - {
+ */
 function BodyMassIndex(props){
 
     
