6b382a17017a7aad26a278a2b43f0bd705ad3311
melaniemdm <melanieborja.pro@gmail.com>
2022-06-15 09:27:34 +0200

    feature(js) documentation dailyActivity

--- src/components/DailyActivity/index.js
+++ src/components/DailyActivity/index.js
@@ -8,17 +8,40 @@ import {
   Tooltip,
   Legend
 } from "recharts";
-
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from 'react-router-dom';
+
+/**
+ * It takes the data from the API and maps it into an array of objects.
+ * </code>
+ * 
+ * 
+ * A:
+ * 
+ * You can use <code>useEffect</code> to fetch the data and set it to the state.
+ * <code>import React, { useState, useEffect } from "react";
+ * import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from "recharts";
+ * import axios from "axios";
+ * 
+ * export default function DailyActivity() {
+ *   const [activities, setActivities] = useState([]);
+ *   const { id } = useParams();
+ * 
+ *   useEffect(() =&gt; {
+ *     const getData = async () =&gt; {
+ *       const { data } = await axios.get(`http://localhost:3000/user/` + id + `/activity`);
+ * @returns The data from the API is being returned.
+ */
 export default function DailyActivity() {
   const [activities, setActivities] = useState([]);
   const {id} = useParams();
+/**
+ * It takes the data from the API and maps it into an array of objects.
+ */
   const getData = async () => {
     const { data } = await axios.get(`http://localhost:3000/user/`+ id +`/activity`);
 
-   
 const activitiesArray = data.data.sessions.map(activity=> { 
     return{
        name: activity.day,
@@ -28,10 +51,11 @@ const activitiesArray = data.data.sessions.map(activity=> {
    
   })
   setActivities(activitiesArray)
-
   }
-
-  
+ 
+ /* A hook that is used for performing side effects in function components. It serves the same purpose
+ as componentDidMount, componentDidUpdate, and componentWillUnmount in React classes, but unified
+ into a single API. */
   useEffect(() => {
     getData();
   });
