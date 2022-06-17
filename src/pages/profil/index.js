cea2c6b822ce39387ddfd6b0652254427cf6dccf
melaniemdm <melanieborja.pro@gmail.com>
2022-06-17 14:54:11 +0200

    fix(js) title profil vs figma

--- src/pages/profil/index.js
+++ src/pages/profil/index.js
@@ -61,9 +61,8 @@ function Profil() {
   });
 
   return (
-    <div className="containerFirst">
-      <div className="titleProfil">
-        Bonjour &nbsp;<div className="name">{name}</div>
+    <div className="containerGraph">
+      <div className="titleProfil">Bonjour &nbsp;<div className="name">{name}</div>
       </div>
       <div className="txtProfil">
         <img src={clappingTxt} alt="logo clapping du texte" />
