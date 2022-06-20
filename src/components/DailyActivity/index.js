59e0ef505e6d3f5a379921e6b788c636e59d9507
melaniemdm <melanieborja.pro@gmail.com>
2022-06-20 17:42:05 +0200

    fix(js) parametre graph activity

--- src/components/DailyActivity/index.js
+++ src/components/DailyActivity/index.js
@@ -11,6 +11,21 @@ import {
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from 'react-router-dom';
+const getBarShape = (x, y, width, height, radius) => {
+  const [tl, tr, bl, br] = radius;
+  const d = `M${x},${y + tl}
+        a${tl},${tl} 0 0 1 ${tl},${-tl}
+        h${width - tl - tr}
+        a${tr},${tr} 0 0 1 ${tr},${tr}
+        v${height - tr - br}
+        a${br},${br} 0 0 1 ${-br},${br}
+        h${bl + (br - width)}
+        a${bl},${bl} 0 0 1 ${-bl},${-bl}
+        z`;
+  return ({ fill, fillOpacity, stroke }) => (
+    <path d={d} fill={fill} fillOpacity={fillOpacity} stroke={stroke} />
+  );
+};
 
 /**
  * It takes the data from the API and maps it into an array of objects.
@@ -80,8 +95,39 @@ const activitiesArray = data.data.sessions.map(activity=> {
       <YAxis yAxisId="right" orientation="right" stroke="gray" />
       <Tooltip />
       <Legend verticalAlign="top" height={36} align="right" iconType="cercle"/>
-      <Bar yAxisId="right" dataKey="kilogram" fill="black" barSize={7} />
-      <Bar yAxisId="left" dataKey="calories" fill="red" barSize={7} />
+      <Bar yAxisId="left" dataKey="calories" fill="black" barSize={7} shape={({ x, y, width, height, value, background }) => {
+        const Bar = getBarShape(x, y, width, height, [4, 4, 0, 0]);
+        return (
+          <g>
+            <rect
+              x={background.x}
+              y={background.y}
+              width={background.width}
+              height={background.height}
+              fill={'transparent'}
+            />
+
+            <Bar fillOpacity={1} fill={'black'} />
+          </g>
+        );
+      }}/>
+      <Bar yAxisId="right" dataKey="kilogram" fill="red" barSize={7} shape={({ x, y, width, height, value, background }) => {
+        const Bar = getBarShape(x, y, width, height, [4, 4, 0, 0]);
+        return (
+          <g>
+            <rect
+              x={background.x}
+              y={background.y}
+              width={background.width}
+              height={background.height}
+              fill={'transparent'}
+            />
+
+            <Bar fillOpacity={1} fill={'red'} />
+          </g>
+        );
+      }}/>
+     
      
     </BarChart></div>
   );
