6c71debc2dc1a5f1beb4c28b329185254472fa78
melaniemdm <melanieborja.pro@gmail.com>
2022-07-27 11:06:44 +0200

    fix(js) api & apiMock constructor

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,90 +1,86 @@
 import axios from 'axios';
 import Models from '../models';
 
+const models = new Models();
 export default class {
   constructor(){
     this.baseUrl= 'http://localhost:3000/';
     this.user_endPoint='user/';
-    /* A function that is called in the component. */
-    this.getAverageSessions = async (id, setStateData) => {
-      await axios.get(
-        this.baseUrl +this.user_endPoint + id + ' /average-sessions'
-      ).then((response) =>{
-        const averageSessions = new Models();
-        const averageSessionsArray = averageSessions.getAverageSessions(response.data.data.sessions);
-        setStateData(averageSessionsArray);
-      }).catch((error) => {
-        console.error(error);
-        setStateData('error');
-
-      });
-     
-    };
-
-    /* A function that is called in the component. */
-    this.getDailyActivity = async (id, setDailyActivity) => {
-      await axios.get(
-        this.baseUrl +this.user_endPoint + id + '/activity'
-      ).then((response) => {
-        const dailyActivity = new Models();
-        const activitiesArray = dailyActivity.getDailyActivity(response.data.data.sessions);
-        setDailyActivity(activitiesArray);
-      }).catch((error) => {
-        console.error(error);
-        setDailyActivity('error');
-      });
-    };
-
-    /* A function that is called in the component. */
-    this.getPerformance = async (id, setPerformance) => {
-      await axios.get(
-        this.baseUrl +this.user_endPoint + id + '/performance'
-      ).then((response) => {
-        const performance = new Models();
-        const performanceArray = performance.getPerformance(response.data.data);
-      
-        setPerformance(performanceArray);
-      }).catch((error) => {
-        console.error(error);
-        setPerformance('error');
-      });
-
-    };
-
-    /* A function that is called in the component. */
-    this.getDataScore = async (id, setScore) => {
-      await axios.get(this.baseUrl +this.user_endPoint + id).then((response) => {
-        const scoreUser = response.data.data.todayScore ? response.data.data.todayScore : response.data.data.score;
-        const score= new Models();
-        const scoreArray= score.getDataScore(scoreUser);
-        setScore(scoreArray);
-      });
-    
-    };
-
-    /* A function that is called in the component. */
-    this.getFirstName = async (id, setFirstName) => {
-      await axios.get(this.baseUrl +this.user_endPoint + id).then((response)=>{
-        setFirstName(response.data.data.userInfos.firstName);
-      }).catch(error=>{
-        console.error(error);
-        setFirstName('error');
-      });
-    };
-
-    /* A function that is called in the component. */
-    this.getBMI = async (id, setBMI) => {
-      await axios.get(this.baseUrl +this.user_endPoint + id).then((response) => {
-        const keyData = response.data.data.keyData;
-        const bmi = new Models();
-        const bmiArray = bmi.getBMI(keyData);
-        setBMI(bmiArray);
-      }).catch(error=> {
-        console.error(error);
-        setBMI('error');
-      });
-     
-    };
+  }
+  /* A function that is called in the component. */
+  async getAverageSessions(id, setStateData)  {
+    await axios.get(
+      this.baseUrl +this.user_endPoint + id + ' /average-sessions'
+    ).then((response) =>{
+   
+      const averageSessionsArray = models.getAverageSessions(response.data.data.sessions);
+      setStateData(averageSessionsArray);
+    }).catch((error) => {
+      console.error(error);
+      setStateData('error');
+    });
+ 
+  }
+  /* A function that is called in the component. */
+  async getDailyActivity (id, setDailyActivity){
+    await axios.get(
+      this.baseUrl +this.user_endPoint + id + '/activity'
+    ).then((response) => {
+   
+      const activitiesArray = models.getDailyActivity(response.data.data.sessions);
+      setDailyActivity(activitiesArray);
+    }).catch((error) => {
+      console.error(error);
+      setDailyActivity('error');
+    });
   }
 
+  /* A function that is called in the component. */
+  async getPerformance(id, setPerformance) {
+    await axios.get(
+      this.baseUrl +this.user_endPoint + id + '/performance'
+    ).then((response) => {
+      
+      const performanceArray = models.getPerformance(response.data.data);
+      
+      setPerformance(performanceArray);
+    }).catch((error) => {
+      console.error(error);
+      setPerformance('error');
+    });
+
+  }
+  /* A function that is called in the component. */
+  async getDataScore(id, setScore) {
+    await axios.get(this.baseUrl +this.user_endPoint + id).then((response) => {
+      const scoreUser = response.data.data.todayScore ? response.data.data.todayScore : response.data.data.score;
+   
+      const scoreArray= models.getDataScore(scoreUser);
+      setScore(scoreArray);
+    });
+
+  }
+  /* A function that is called in the component. */
+  async getFirstName(id, setFirstName) {
+    await axios.get(this.baseUrl +this.user_endPoint + id).then((response)=>{
+      setFirstName(response.data.data.userInfos.firstName);
+    }).catch(error=>{
+      console.error(error);
+      setFirstName('error');
+    });
+  }
+
+  /* A function that is called in the component. */
+  async getBMI  (id, setBMI){
+    await axios.get(this.baseUrl +this.user_endPoint + id).then((response) => {
+      const keyData = response.data.data.keyData;
+    
+      const bmiArray = models.getBMI(keyData);
+      setBMI(bmiArray);
+    }).catch(error=> {
+      console.error(error);
+      setBMI('error');
+    });
+ 
+  }
 }
--- src/utils/apiMock.js
+++ src/utils/apiMock.js
@@ -1,174 +1,179 @@
 import Models from '../models';
+
+const models = new Models();
 export default class {
   constructor(){
     
-    /* A function that is being called in the constructor. */
-    this.getAverageSessions = async ( setStateData) => {
-      const data = {
-        'data': {
-          'userId': 18,
-          'sessions': [
-            {
-              'day': 1,
-              'sessionLength': 30
-            },
-            {
-              'day': 2,
-              'sessionLength': 40
-            },
-            {
-              'day': 3,
-              'sessionLength': 50
-            },
-            {
-              'day': 4,
-              'sessionLength': 30
-            },
-            {
-              'day': 5,
-              'sessionLength': 30
-            },
-            {
-              'day': 6,
-              'sessionLength': 50
-            },
-            {
-              'day': 7,
-              'sessionLength': 50
-            }
-          ]
-        }
-      };
-      const averageSessions = new Models();
-      const averageSessionsArray = averageSessions.getAverageSessions(data.data.sessions);
-      setStateData(averageSessionsArray);
-      
-    };
-
-    /* A function that is being called in the constructor. */
-    this.getDailyActivity = async (id, setDailyActivity) => {
-      const data={
-        'data': {
-          'userId': 18,
-          'sessions': [
-            {
-              'day': '2020-07-01',
-              'kilogram': 70,
-              'calories': 240
-            },
-            {
-              'day': '2020-07-02',
-              'kilogram': 69,
-              'calories': 220
-            },
-            {
-              'day': '2020-07-03',
-              'kilogram': 70,
-              'calories': 280
-            },
-            {
-              'day': '2020-07-04',
-              'kilogram': 70,
-              'calories': 500
-            },
-            {
-              'day': '2020-07-05',
-              'kilogram': 69,
-              'calories': 160
-            },
-            {
-              'day': '2020-07-06',
-              'kilogram': 69,
-              'calories': 162
-            },
-            {
-              'day': '2020-07-07',
-              'kilogram': 69,
-              'calories': 390
-            }
-          ]
-        }
-      };
-
-      const dailyActivity = new Models();
-      const dailyActivityArray = dailyActivity.getDailyActivity(data.data.sessions);
-      setDailyActivity(dailyActivityArray);
-    };
-
-    /* A function that is being called in the constructor. */
-    this.getPerformance = async (id,setPerformance) => {
-      const data={
-        'data': {
-          'userId': 12,
-          'kind': {
-            '1': 'cardio',
-            '2': 'energy',
-            '3': 'endurance',
-            '4': 'strength',
-            '5': 'speed',
-            '6': 'intensity'
-          },
-          'data': [
-            {
-              'value': 80,
-              'kind': 1
-            },
-            {
-              'value': 120,
-              'kind': 2
-            },
-            {
-              'value': 140,
-              'kind': 3
-            },
-            {
-              'value': 50,
-              'kind': 4
-            },
-            {
-              'value': 200,
-              'kind': 5
-            },
-            {
-              'value': 90,
-              'kind': 6
-            }
-          ]
-        }
-      };
-      const performance = new Models();
-      const performanceArray = performance.getPerformance(data.data.sessions);
-      setPerformance(performanceArray);
-    };
-
-    /* A function that is being called in the constructor. */
-    this.getDataScore = async (id, setScore) => {
-      const score = new Models();
-      const scoreArray = score.getScore(0.12);
-      setScore(scoreArray);
-    };
-
-    /* A function that is being called in the constructor. */
-    this.getFirstName = async (id, setFirstName) => {
-      setFirstName('Karl');
-    };
-
-    /* A function that is being called in the constructor. */
-    this.getBMI = async (id, setBMI) => {
-      const keyData= {
-        'calorieCount': 2500,
-        'proteinCount': 90,
-        'carbohydrateCount': 150,
-        'lipidCount': 120
-      };
-      const bmi=new Models();
-      const bmiArray = bmi.getBMI(keyData);
-      setBMI(bmiArray);
-    
-    };
-
-
 
   }
+
+  /* A function that is being called in the constructor. */
+  async getAverageSessions( setStateData) {
+    const data = {
+      'data': {
+        'userId': 18,
+        'sessions': [
+          {
+            'day': 1,
+            'sessionLength': 30
+          },
+          {
+            'day': 2,
+            'sessionLength': 40
+          },
+          {
+            'day': 3,
+            'sessionLength': 50
+          },
+          {
+            'day': 4,
+            'sessionLength': 30
+          },
+          {
+            'day': 5,
+            'sessionLength': 30
+          },
+          {
+            'day': 6,
+            'sessionLength': 50
+          },
+          {
+            'day': 7,
+            'sessionLength': 50
+          }
+        ]
+      }
+    };
+
+    const averageSessionsArray = models.getAverageSessions(data.data.sessions);
+    setStateData(averageSessionsArray);
+  
+  }
+
+  /* A function that is being called in the constructor. */
+  async getDailyActivity (id, setDailyActivity){
+    const data={
+      'data': {
+        'userId': 18,
+        'sessions': [
+          {
+            'day': '2020-07-01',
+            'kilogram': 70,
+            'calories': 240
+          },
+          {
+            'day': '2020-07-02',
+            'kilogram': 69,
+            'calories': 220
+          },
+          {
+            'day': '2020-07-03',
+            'kilogram': 70,
+            'calories': 280
+          },
+          {
+            'day': '2020-07-04',
+            'kilogram': 70,
+            'calories': 500
+          },
+          {
+            'day': '2020-07-05',
+            'kilogram': 69,
+            'calories': 160
+          },
+          {
+            'day': '2020-07-06',
+            'kilogram': 69,
+            'calories': 162
+          },
+          {
+            'day': '2020-07-07',
+            'kilogram': 69,
+            'calories': 390
+          }
+        ]
+      }
+    };
+
+
+    const dailyActivityArray = models.getDailyActivity(data.data.sessions);
+    setDailyActivity(dailyActivityArray);
+  }
+
+  /* A function that is being called in the constructor. */
+  async getPerformance(id,setPerformance){
+    const data={
+      'data': {
+        'userId': 12,
+        'kind': {
+          '1': 'cardio',
+          '2': 'energy',
+          '3': 'endurance',
+          '4': 'strength',
+          '5': 'speed',
+          '6': 'intensity'
+        },
+        'data': [
+          {
+            'value': 80,
+            'kind': 1
+          },
+          {
+            'value': 120,
+            'kind': 2
+          },
+          {
+            'value': 140,
+            'kind': 3
+          },
+          {
+            'value': 50,
+            'kind': 4
+          },
+          {
+            'value': 200,
+            'kind': 5
+          },
+          {
+            'value': 90,
+            'kind': 6
+          }
+        ]
+      }
+    };
+
+    const performanceArray = models.getPerformance(data.data.sessions);
+    setPerformance(performanceArray);
+  }
+
+  /* A function that is being called in the constructor. */
+  async getDataScore(id, setScore) {
+ 
+    const scoreArray = models.getScore(0.12);
+    setScore(scoreArray);
+  }
+
+  /* A function that is being called in the constructor. */
+  async getFirstName(id, setFirstName) {
+    setFirstName('Karl');
+  }
+
+  /* A function that is being called in the constructor. */
+  async getBMI(id, setBMI){
+    const keyData= {
+      'calorieCount': 2500,
+      'proteinCount': 90,
+      'carbohydrateCount': 150,
+      'lipidCount': 120
+    };
+  
+    const bmiArray = models.getBMI(keyData);
+    setBMI(bmiArray);
+
+  }
+
+
+
+
 
 }
